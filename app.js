bf650fec0e90fecffcbc1cac7d7da8e8bd09002f
Jeremiah Akpera <jerryakpera@gmail.com>
2020-02-20 12:02:52 +0300

    v0.3.1 Express error handler

--- app.js
+++ app.js
@@ -20,4 +20,22 @@ const baseURL = "/api/v1"
 const authRoute = require("./api/auth/auth")
 app.use(`${baseURL}/auth`, authRoute)
 
+// ERROR Middleware
+// Handles routes that are not handled by our application
+app.use((req, res, next) => {
+  const err = new Error("Not found!")
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    }
+  })
+})
+
 module.exports = app
